cb7dddfe1fae19b42c6e4ec4210fdf39ebec5435
AkiraTomori <ThaiMinhHuy1407@gmail.com>
2024-11-14 15:47:38 +0700

    hai hàm shutdown, restart đã được đóng gói vào app.cpp

--- Server/src/Server.cpp
+++ Server/src/Server.cpp
@@ -29,56 +29,6 @@ bool Server::keylog(Request& request, Response &response) {
     return true;
 }
 
-void Server::shutdownSystem(){
-    HANDLE hToken;
-    TOKEN_PRIVILEGES tkp;
-    
-    // Lấy token cho tiến trình hiện tại
-    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)){
-        return;
-    }
-    // Lấy LUID cho đặc quyền shutdown
-    LookupPrivilegeValue(nullptr, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid);
-    tkp.PrivilegeCount = 1; // one privillege to set;
-    tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-    
-    // Kích hoạt đặc quyền shutdown cho tiến trình
-    AdjustTokenPrivileges(hToken, false, &tkp, 0, (PTOKEN_PRIVILEGES)nullptr, 0);
-    if (GetLastError() != ERROR_SUCCESS)
-        return;
-    
-    // Tắt hệ thống, buộc đóng tất cả ứng dụng
-    if (!ExitWindowsEx(EWX_SHUTDOWN | EWX_FORCE, SHTDN_REASON_MAJOR_OTHER)){
-        std::cout << "Shutdown failed: Error: " << GetLastError() << "\n";
-    }
-    std::cout << "System is shutting down.....\n";
-}
-void Server::restartSystem(){
-    HANDLE hToken;
-    TOKEN_PRIVILEGES tkp;
-
-    // Lấy token cho tiến trình hiện tại
-    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)) {
-        return;
-    }
-
-    // Lấy LUID cho đặc quyền shutdown
-    LookupPrivilegeValue(nullptr, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid);
-    tkp.PrivilegeCount = 1;
-    tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-
-    // Kích hoạt đặc quyền shutdown cho tiến trình
-    AdjustTokenPrivileges(hToken, false, &tkp, 0, (PTOKEN_PRIVILEGES)nullptr, 0);
-    if (GetLastError() != ERROR_SUCCESS) {
-        return;
-    }
-
-    // Khởi động lại hệ thống, buộc đóng tất cả ứng dụng
-    if (!ExitWindowsEx(EWX_REBOOT | EWX_FORCE, SHTDN_REASON_MAJOR_OTHER)) {
-        std::cout << "Restart failed: Error: " << GetLastError() << "\n";
-    }
-    std::cout << "System is restarting.....\n";
-}
 bool Server::processRequest(Request& request, Response &response) {
     cout << "Processing Request\n" << request << "\n";
     response.setAction(request.getAction());
@@ -87,16 +37,6 @@ bool Server::processRequest(Request& request, Response &response) {
     switch (request.getAction()) {
         case ACTION_KEYLOG:
             return keylog(request, response);
-        case ACTION_SHUTDOWN:
-            shutdownSystem();
-            response.putParam("Status", "Success");
-            response.putParam("Body", "System is shutting down.");
-            return true;
-        case ACTION_RESTART:
-            restartSystem();
-            response.putParam("Status", "Success");
-            response.putParam("Body", "System is reset.");
-            return true;
         default:
             return false;
     }
